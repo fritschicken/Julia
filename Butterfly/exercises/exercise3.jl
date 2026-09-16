48d9953e2f894f53bd1747cf7c07e3250c4457a9
Laszlo Fritsch <fryc02@outlook.com>
2026-09-16 15:58:53 +0200

    Fixed unit cell creation

--- Butterfly/exercises/exercise3.jl
+++ Butterfly/exercises/exercise3.jl
@@ -1,18 +1,24 @@
 using Quantica, GLMakie, StaticArrays
-a = 1
+a = 2
 N = 4
-M=1
-L = (N-1) * M
+M=3
 
-h = LP.square() |> supercell(region = r -> ((r[1] % (N-1) == 0 &&  0<=r[1] <= L && 0 <= r[2] <= L) || 
-                           (r[2] % (N-1) == 0 &&  0<=r[2] <= L && 0 <= r[1] <= L)))
-h = h |> (@onsite((; ϵ = 0.0) -> ϵ) + @hopping((r, dr; t = 1.0, B = 0.0) -> t * cis(-B * r[2] * dr[1]), range = 1.0))
+function unit_coords(N,a)
+    coords = Tuple{Number, Number}[(0,0)]
+    for i in 1:N-2
+        push!(coords, (0, a*i/(N-1)))
+        push!(coords, (a*i/(N-1), 0))
+    end
+    return coords
+end
 
+pts = unit_coords(N,a)
+lat = lattice(sublat(pts), bravais=([0,a], [a,0]))
+sl = lat |> supercell(region = r -> (0 <= r[1] <= M*a) && (0 <= r[2] <= M*a))
+h = sl |> (@onsite((; ϵ = 0.0) -> ϵ) + @hopping((r, dr; t = 1.0, B = 0.0) -> t * cis(-B * r[2] * dr[1])))
 
 Bs = range(-pi/2,pi/2, length=199)
 params = (;ϵ=0, t=2, B=0)
 b = bands(h, Bs; mapping = B ->ftuple(;params...,B))
-
-
-qplot(h(B=0.1))
-qplot(b,hide=:nodes)
+qplot(h)
+#qplot(b,hide=:nodes)
